b5af152dbc81296a967edaaca402db1f6cfc1a96
bethgetachew <bethgetachew@icloud.com>
2019-04-15 12:37:57 +0100

    Added mobile styles

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 /* jshint esversion:6 */
 
-const bigTitleTag = document.querySelector(".big-title");
+const h1Tag = document.querySelector(".big-title");
 const fixedNavTag = document.querySelector(".fixed");
 const hiddenTextTag = document.querySelector(".hidden-text");
 const navTag = document.querySelector("nav");
 
 const fadeInTitle = function() {
-  bigTitleTag.style.top = "50%";
+  h1Tag.style.top = "50%";
 };
 
 window.onload = fadeInTitle();
@@ -16,11 +16,11 @@ document.addEventListener("scroll", function() {
   let distanceFromTop = window.scrollY;
 
   if (distanceFromTop > 3000) {
-    bigTitleTag.style.opacity = "0";
+    h1Tag.style.opacity = "0";
     navTag.style.opacity = "0";
     hiddenTextTag.style.bottom = "35%";
   } else {
-    bigTitleTag.style.opacity = "1";
+    h1Tag.style.opacity = "1";
     navTag.style.opacity = "1";
     hiddenTextTag.style.bottom = "-1000px";
   }
